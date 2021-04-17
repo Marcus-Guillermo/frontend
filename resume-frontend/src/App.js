a54453b9943d4ae306dd036da8df146787cc5165
mgtz505 <gutierrez.marcusc@gmail.com>
2021-04-17 16:03:35 -0400

    Test Render and Commit

---
+++ resume-frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          TEST RENDER
+        </p>
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
