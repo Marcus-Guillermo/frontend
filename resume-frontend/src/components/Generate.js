f937dd6f7279d35cabac6519e3b9847f160c00bd
mgtz505 <gutierrez.marcusc@gmail.com>
2021-04-19 12:32:00 -0400

    PDF is formatted

--- resume-frontend/src/components/Generate.js
+++ resume-frontend/src/components/Generate.js
@@ -17,21 +17,33 @@ const Generate = () => {
             paddingBottom: 65,
             paddingHorizontal: 35,
         },
-        title: {
-            fontSize: 48,
-            marginTop: 60,
-            textAlign: "center"},
-
-        header: {
-            fontSize: 52,
-            marginBottom: 20,
-            textAlign: 'center'
-        },
+       
         section: {
-          margin: 20,
+            marginTop: 10,
+          marginLeft: 20,
           padding: 0,
-          flexGrow: 1
+        //   flexGrow: 1,
+          fontSize: 14
         },
+        myContact: {
+            fontSize: 14
+        },
+        date : {
+            marginTop: 40,
+            fontSize: 14
+        },
+        salutation: {
+            marginTop: 20,
+            fontSize: 14
+        },
+        intro: {
+
+        },
+        signOff: {
+            marginLeft: 20,
+            marginTop: 40,
+            fontSize: 14
+        }
         
       });
 
@@ -42,16 +54,32 @@ const Generate = () => {
     <PDFViewer className="viewer">
         <Document>
         <Page size="A4" style={styles.body}>
-            <Text style={styles.header} fixed>
-                Created with React-pdf
-            </Text>
-            <Text style={styles.title}>TITLE EXAMPLE TEXT HERE</Text>
-        <View style={styles.section}>
-            <Text>SECTION</Text>
-        </View>
-        <View style={styles.section}>
-            <Text></Text>
-        </View>
+
+            {/* USER CONTACT INFORMATION */}
+            <Text style={styles.myContact}> Marvin Example</Text>
+            <Text style={styles.myContact}> 12 Shock Road, Hinderland, NY 84560</Text>
+            <Text style={styles.myContact}> Cell: (555) 474-65378</Text>
+            <Text style={styles.myContact}> marvbale@example.com</Text>
+            {/* Today's Date */}
+            <Text style={styles.date}> April 19th, 2021</Text>
+
+            {/* Salutation */}
+            <Text style={styles.salutation}> Dear Hiring Manager,</Text>
+
+            {/* Intro Paragraph */}
+            <Text style={styles.section}> It is with deep interest that I submit my application for the Software Engineer post as advertised on softtoolhr.com. The position is definitely an ideal chance in view if my strong technical expertise, over 10 years' experience in various areas across security, network engineering, software coding, architecture and IT project supervision and management.</Text>
+
+            {/* Body Paragraph */}
+            <Text style={styles.section}> I am currently the Jackheat South Town Technologies Chief Software Engineer, a role I have held for six years. In this position I lead and supervise technology projects, performance and quality enhancements and craft product offers with an impact on the organization's bottom line. Through my increasing experience and progressive professional development my managed skills in IT have been refined and business insight enhanced resulting in inspired IT solutions, improved profits and cost reductions. All these accomplishments are directly through successful team work and collaborations with diverse stakeholders, efficient team management and calculated allocation of resources.</Text>
+
+            {/* Call to Action */}
+            <Text style={styles.section}> The enclosed CV offers a detailed picture of my accomplishments, expertise and qualifications in Software Engineering. I am reachable by mail or phone to discuss your software engineering needs and expectations and how my skills can meet these requirements</Text>
+
+            {/* Sign-Off */}
+            <Text style={styles.signOff}> I look forward to speaking with you soon.</Text>
+            <Text style={styles.signOff}> Marvin Example</Text>
+
+    
         </Page>
     </Document>
     </PDFViewer>
