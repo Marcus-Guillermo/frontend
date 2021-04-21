83962c987dac39586a11c8ba015a4ca27a4a7954
mgtz505 <gutierrez.marcusc@gmail.com>
2021-04-21 14:35:10 -0400

    Getting ready to merge w/ main

--- resume-frontend/src/components/Homepage.js
+++ resume-frontend/src/components/Homepage.js
@@ -18,16 +18,20 @@ const Homepage = () => {
             <h1 className="welcomeBanner">Resu.Me</h1>
             <h4 className="welcomeMessage">Resu.me is the easiest solution for cover letters and resumes.
                 <br></br> Simply input your credentials and contact information - We handle the rest!</h4>
+                <Button onClick={handleShow} className="loginButton" >Log In to Resu.me</Button>
+                
                 <Link className= "genLeadButton" to="/generate-resume">
                 <Button >Let's make a Resume</Button>
                 </Link>
       
                 
-                <Button onClick={handleShow} className="loginButton" >Log In to Resu.me</Button>
             {/* <h2 className="servicesBanner">Score the job you deserve with the help of our tools</h2> */}
             <Link className= "genLeadTwoButton" to="/generate">
-                <Button >Let's make a Cover Letter</Button>
-                </Link>
+              <Button >Let's make a Cover Letter</Button>
+            </Link>
+            <Link className= "genLeadButton" to="/generate-resume">
+              <Button >Let's make a Resume</Button>
+            </Link>
            
                 <h2 className="servicesContainer serviceA">Convenient Cover Letters</h2>
                 <h4 className="servicesDescription">Simply enter your contact information and several brief sentences about the role you want. <strong>We handle all of the formatting and styling</strong>.</h4>
